bb8d326aaa1968dbe68d936e54b1d698bf26c8fa
Panamal74 <pavlo.malakhovskyi@gmail.com>
2018-05-17 15:11:49 +0300

    Тесты функций +

--- source/instruments/helpers.js
+++ source/instruments/helpers.js
@@ -2,57 +2,73 @@ export function getDisplayName (WrappedComponent) {
     return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
 
-// export function getShowTasks (searchValue, tasks) {
-//     const filterTasks = tasks.filter((value) =>
-//         value.message.toUpperCase().indexOf(searchValue.toUpperCase()) !== -1
-//     );
-//     const favoriteTasks = filterTasks.filter((value) =>
-//         value.favorite === true && value.completed === false
-//     );
-//     const completedFavoriteTasks = filterTasks.filter((value) =>
-//         value.completed === true && value.favorite === true
-//     );
-//     const completedOtherTasks = filterTasks.filter((value) =>
-//         value.completed === true && value.favorite === false
-//     );
-//     const otherTasks = filterTasks.filter((value) =>
-//         value.completed === false && value.favorite === false
-//     );
-//
-//     return favoriteTasks.concat(otherTasks, completedFavoriteTasks, completedOtherTasks);
-// }
+export function getFilterTasks (searchValue = '', tasks = []) {
+    if (typeof searchValue !== "string") {
+        throw new Error("Первый аргумент функции должен иметь тип string");
+    } else if (!Array.isArray(tasks)) {
+        throw new Error("Второй аргумент функции должен иметь тип array");
+    }
 
-export function getFilterTasks (searchValue, tasks) {
-    return tasks.filter((value) =>
-        value.message.toUpperCase().indexOf(searchValue.toUpperCase()) !== -1
-    );
+    let returnValue = [];
+
+    if (searchValue) {
+        returnValue = tasks.filter((value) =>
+            value.message.toUpperCase().indexOf(searchValue.toUpperCase()) !== -1
+        );
+    } else {
+        returnValue = tasks;
+    }
+
+    return returnValue;
 }
 
-export function getFavoriteTasks (filterTasks) {
+export function getFavoriteTasks (filterTasks = []) {
+    if (!Array.isArray(filterTasks)) {
+        throw new Error("Аргумент функции должен иметь тип array");
+    }
+
     return filterTasks.filter((value) =>
         value.favorite === true && value.completed === false
     );
 }
 
-export function getCompletedFavoriteTasks (filterTasks) {
+export function getCompletedFavoriteTasks (filterTasks = []) {
+    if (!Array.isArray(filterTasks)) {
+        throw new Error("Аргумент функции должен иметь тип array");
+    }
+
     return filterTasks.filter((value) =>
         value.completed === true && value.favorite === true
     );
 }
 
-export function getCompletedOtherTasks (filterTasks) {
+export function getCompletedOtherTasks (filterTasks = []) {
+    if (!Array.isArray(filterTasks)) {
+        throw new Error("Аргумент функции должен иметь тип array");
+    }
+
     return filterTasks.filter((value) =>
         value.completed === true && value.favorite === false
     );
 }
 
-export function getOtherTasks (filterTasks) {
+export function getOtherTasks (filterTasks = []) {
+    if (!Array.isArray(filterTasks)) {
+        throw new Error("Аргумент функции должен иметь тип array");
+    }
+
     return filterTasks.filter((value) =>
         value.completed === false && value.favorite === false
     );
 }
 
-export function checkFieldLength (value, maxLength = 50) {
+export function checkFieldLength (value = '', maxLength = 50) {
+    if (typeof value !== "string") {
+        throw new Error("Первый аргумент функции должен иметь тип string");
+    } else if (typeof maxLength !== "number") {
+        throw new Error("Второй аргумент функции должен иметь тип number");
+    }
+
     return value.length > maxLength
         ? value.slice(0, maxLength)
         : value;
--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -1,54 +1,207 @@
-import { getDisplayName, getFavoriteTasks } from './helpers';
-import defaultTasks from './tasks';
+import {
+    getDisplayName,
+    getFilterTasks,
+    getFavoriteTasks,
+    getCompletedFavoriteTasks,
+    getCompletedOtherTasks,
+    getOtherTasks,
+    checkFieldLength
+} from './helpers';
+import tasks from './tasks';
 
-describe('test getDisplayName function', () => {
-    test('getDisplayName function should return is not string', () => {
+const defaultTasks = tasks.tasks;
+
+describe('Тест функции getDisplayName', () => {
+    test('Функция getDisplayName должна возвращать строку', () => {
         expect(typeof getDisplayName({})).toBe('string');
     });
 
-    test('getDisplayName function should return displayName', () => {
+    test('Функция getDisplayName должна возвращать `name`', () => {
         expect(getDisplayName({ displayName: 'name' })).toBe('name');
     });
 
-    test('getDisplayName function should return name', () => {
+    test('Функция getDisplayName должна возвращать `name`', () => {
         expect(getDisplayName({ name: 'name' })).toBe('name');
     });
 
-    test('getDisplayName function should return Component', () => {
+    test('Функция getDisplayName должна возвращать компонент', () => {
         expect(getDisplayName({})).toBe('Component');
     });
 
-    test('getDisplayName function should return undefined', () => {
+    test('Функция getDisplayName не должна возвращать undefined', () => {
         expect(getDisplayName({ displayName: 'name' })).not.toBeUndefined();
     });
 
-    test('getDisplayName function should return undefined', () => {
+    test('Функция getDisplayName не должна возвращать undefined', () => {
         expect(getDisplayName({ name: 'name' })).not.toBeUndefined();
     });
 
-    test('getDisplayName function should return undefined', () => {
+    test('Функция getDisplayName не должна возвращать undefined', () => {
         expect(getDisplayName({})).not.toBeUndefined();
     });
 });
 
-describe('test getFavoriteTasks function', () => {
-    test('getFavoriteTasks function should return is not array', () => {
-        expect(Array.isArray(getFavoriteTasks([]))).toBe(true);
+describe('Тест функции getFilterTasks', () => {
+    test('Первый аргумент функции checkFieldLength должен иметь тип string', () => {
+        expect(() => getFilterTasks(1, [])).toThrow('Первый аргумент функции должен иметь тип string');
     });
 
-    test('getFavoriteTasks function should result 2 tasks favorite in json', () => {
-        expect(getFavoriteTasks(defaultTasks.tasks).length).toBe(2);
+    test('Второй аргумент функции getFilterTasks должен иметь тип array', () => {
+        expect(() => getFilterTasks('str', 'str')).toThrow('Второй аргумент функции должен иметь тип array');
     });
 
-    test('getFavoriteTasks function should result 2 tasks favorite in json', () => {
-        expect(getFavoriteTasks(defaultTasks.tasks).length).not.toBe(0);
+    test('Функция getFilterTasks не должна возвращать undefined', () => {
+        expect(getFilterTasks()).not.toBeUndefined();
     });
 
-    test('getFavoriteTasks function should return undefined', () => {
-        expect(getFavoriteTasks(defaultTasks.tasks)).not.toBeUndefined();
+    test('Функция getFilterTasks должна возвращать массив', () => {
+        expect(Array.isArray(getFilterTasks())).toBe(true);
     });
 
-    test('getFavoriteTasks function should return object in array ', () => {
-        expect(typeof getFavoriteTasks(defaultTasks.tasks)[0]).toBe('object');
+    test('Функция getFilterTasks должна возвращать массив объектов', () => {
+        expect(typeof getFilterTasks('', defaultTasks)).toBe('object');
     });
+
+    test('Функция getFilterTasks должна возвращать не пустой массив', () => {
+        expect(getFilterTasks('', defaultTasks).length).not.toBe(0);
+    });
+
+    test('Функция getFilterTasks должна возвращать массив из трёх элементов', () => {
+        expect(getFilterTasks('ба', defaultTasks).length).toBe(3);
+    });
+});
+
+describe('Тест функции getFavoriteTasks', () => {
+    test('Аргумент функции getFavoriteTasks должен иметь тип array', () => {
+        expect(() => getFavoriteTasks('str')).toThrow('Аргумент функции должен иметь тип array');
+    });
+
+    test('Функция getFavoriteTasks не должна возвращать undefined', () => {
+        expect(getFavoriteTasks()).not.toBeUndefined();
+    });
+
+    test('Функция getFavoriteTasks должна возвращать массив', () => {
+        expect(Array.isArray(getFavoriteTasks())).toBe(true);
+    });
+
+    test('Функция getFavoriteTasks должна возвращать массив объектов', () => {
+        expect(typeof getFavoriteTasks(defaultTasks)[0]).toBe('object');
+    });
+
+    test('Функция getFavoriteTasks не должна возвращать пустой массив', () => {
+        expect(getFavoriteTasks(defaultTasks).length).not.toBe(0);
+    });
+
+    test('Функция getFavoriteTasks должна возвращать массив из двух элементов', () => {
+        expect(getFavoriteTasks(defaultTasks).length).toBe(2);
+    });
+});
+
+describe('Тест функции getCompletedFavoriteTasks', () => {
+    test('Аргумент функции getCompletedFavoriteTasks должен иметь тип array', () => {
+        expect(() => getCompletedFavoriteTasks('str')).toThrow('Аргумент функции должен иметь тип array');
+    });
+
+    test('Функция getCompletedFavoriteTasks не должна возвращать undefined', () => {
+        expect(getCompletedFavoriteTasks()).not.toBeUndefined();
+    });
+
+    test('Функция getCompletedFavoriteTasks должна возвращать массив', () => {
+        expect(Array.isArray(getCompletedFavoriteTasks())).toBe(true);
+    });
+
+    test('Функция getCompletedFavoriteTasks должна возвращать массив объектов', () => {
+        expect(typeof getCompletedFavoriteTasks(defaultTasks)[0]).toBe('object');
+    });
+
+    test('Функция getCompletedFavoriteTasks не должна возвращать пустой массив', () => {
+        expect(getCompletedFavoriteTasks(defaultTasks).length).not.toBe(0);
+    });
+
+    test('Функция getCompletedFavoriteTasks должна возвращать массив из одного элемента', () => {
+        expect(getCompletedFavoriteTasks(defaultTasks).length).toBe(1);
+    });
+});
+
+describe('Тест функции getCompletedOtherTasks', () => {
+    test('Аргумент функции getCompletedOtherTasks должен иметь тип array', () => {
+        expect(() => getCompletedOtherTasks('str')).toThrow('Аргумент функции должен иметь тип array');
+    });
+
+    test('Функция getCompletedOtherTasks не должна возвращать undefined', () => {
+        expect(getCompletedOtherTasks()).not.toBeUndefined();
+    });
+
+    test('Функция getCompletedOtherTasks должна возвращать массив', () => {
+        expect(Array.isArray(getCompletedOtherTasks())).toBe(true);
+    });
+
+    test('Функция getCompletedOtherTasks должна возвращать массив объектов', () => {
+        expect(typeof getCompletedOtherTasks(defaultTasks)[0]).toBe('object');
+    });
+
+    test('Функция getCompletedOtherTasks не должна возвращать пустой массив', () => {
+        expect(getCompletedOtherTasks(defaultTasks).length).not.toBe(0);
+    });
+
+    test('Функция getCompletedOtherTasks должна возвращать массив из одного элемента', () => {
+        expect(getCompletedOtherTasks(defaultTasks).length).toBe(1);
+    });
+});
+
+describe('Тест функции getOtherTasks', () => {
+    test('Аргумент функции getOtherTasks должен иметь тип array', () => {
+        expect(() => getOtherTasks('str')).toThrow('Аргумент функции должен иметь тип array');
+    });
+
+    test('Функция getOtherTasks не должна возвращать undefined', () => {
+        expect(getOtherTasks()).not.toBeUndefined();
+    });
+
+    test('Функция getOtherTasks должна возвращать массив', () => {
+        expect(Array.isArray(getOtherTasks())).toBe(true);
+    });
+
+    test('Функция getOtherTasks должна возвращать массив объектов', () => {
+        expect(typeof getOtherTasks(defaultTasks)[0]).toBe('object');
+    });
+
+    test('Функция getOtherTasks не должна возвращать пустой массив', () => {
+        expect(getOtherTasks(defaultTasks).length).not.toBe(0);
+    });
+
+    test('Функция getOtherTasks должна возвращать массив из одного элемента', () => {
+        expect(getOtherTasks(defaultTasks).length).toBe(1);
+    });
+});
+
+describe('Тест функции checkFieldLength', () => {
+    test('Первый аргумент функции checkFieldLength должен иметь тип string', () => {
+        expect(() => checkFieldLength(1, 2)).toThrow('Первый аргумент функции должен иметь тип string');
+    });
+
+    test('Второй аргумент функции checkFieldLength должен иметь тип number', () => {
+        expect(() => checkFieldLength('str', 'str')).toThrow('Второй аргумент функции должен иметь тип number');
+    });
+
+    test('Функция checkFieldLength должна возвращать значение типа string', () => {
+        expect(typeof checkFieldLength()).toBe('string');
+    });
+
+    test('Функция checkFieldLength при любой длине аргумента вернёт значение длиной до 50 символов включительно', () => {
+        expect(checkFieldLength('').length > 50).toBeFalsy();
+    });
+
+    test('Функция checkFieldLength при любой длине аргумента вернёт значение длиной до 50 символов включительно', () => {
+        expect(checkFieldLength('1111111111222222222233333333334444444444555555555566666666667777777777').length > 50).toBeFalsy();
+    });
+
+    test('Функция checkFieldLength вернёт значение из первых 50 символов', () => {
+        expect(checkFieldLength('1111111111222222222233333333334444444444555555555566666666667777777777')).toBe('11111111112222222222333333333344444444445555555555');
+    });
+
+    test('Функция checkFieldLength вернёт значение из первых 20 символов', () => {
+        expect(checkFieldLength('1111111111222222222233333333334444444444555555555566666666667777777777', 20)).toBe('11111111112222222222');
+    });
+
 });
